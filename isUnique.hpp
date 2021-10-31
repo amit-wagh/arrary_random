6721518eda503a3e5c2ea60e6369ad04036f6b5d
amit-wagh <85372431+amit-wagh@users.noreply.github.com>
2021-10-31 10:51:51 -0400

    Update isUnique.hpp

--- isUnique.hpp
+++ isUnique.hpp
@@ -1,8 +1,8 @@
-auto isUnique( const std::string& input ) noexcept( true ) -> bool {
+auto isUnique( std::string const & input ) noexcept( true ) -> bool {
     std::array<bool, 128> charset{};
     const char zero { '0' };
     for( auto const &c : input ) {
-        if(charset[c - zero])
+        if( charset[c - zero] )
             return false;
         charset[c - zero] = true;
     }
@@ -13,5 +13,5 @@ auto isUnique( const std::string& input ) noexcept( true ) -> bool {
 
 auto difference(std::vector<int> const & input) -> int {
     const auto [ min, max ] = std::minmax_element( std::cbegin( input ), std::cend( input ) );
-    return ( *max - *min);
+    return ( *max - *min );
 }
