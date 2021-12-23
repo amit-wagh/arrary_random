f9549b8681250da041aa27b685e86394a39f022f
Amit Wagh <vishuwagh@gmail.com>
2021-12-23 15:21:24 -0500

    Update evenOdd.hpp

--- evenOdd.hpp
+++ evenOdd.hpp
@@ -1,16 +1,36 @@
-auto evenOdd( std::vector<unsigned int>& input ) -> void {
-    std::size_t nextEven = 0;
-    std::size_t nextOdd  = input.size() - 1;
+#include <cstdlib>
+#include <iostream>
+#include <vector>
 
-    auto isEven = []( const auto & value ) { return value % 2 == 0; };
+#include <iterator>
+namespace {
+    using std::cout;
+    using std::vector;
+    auto constexpr nl {'\n'};
 
-    while( nextEven < nextOdd ) {
-        if( isEven(input[nextEven]) ) {
-            ++nextEven;
-        }
-        else {
-            std::swap( input[nextEven], input[nextOdd]);
-            --nextOdd;
+    auto evenOdd( std::vector<std::int32_t>& input ) noexcept -> void {
+        auto nextOdd  = input.size() - 1;
+        decltype(nextOdd) nextEven = 0;
+        
+        auto constexpr isEven = []( const auto value ) { return value % 2 == 0; };
+
+        while( nextEven < nextOdd ) {
+            if( isEven(input[nextEven]) ) {
+                ++nextEven;
+            }
+            else {
+                std::swap( input[nextEven], input[nextOdd]);
+                --nextOdd;
+            }
         }
     }
+}
+
+
+[[nodiscard]]
+auto main() noexcept -> int {
+    auto input = vector<std::int32_t>{ 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    evenOdd(input);
+    std::copy(cbegin(input), cend(input), std::ostream_iterator<std::int32_t>(cout, " "));
+    return EXIT_SUCCESS;
 }
